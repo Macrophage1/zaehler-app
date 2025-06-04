262bec5dba382b55c120f412473b2ca069ae2d16
Johannes Zabka <johannes.zabka@googlemail.com>
2025-06-04 23:09:03 +0200

    Fix: Login über session_state ausgewertet

--- login.py
+++ login.py
@@ -16,8 +16,13 @@ def login():
 
     name = authenticator.login(location='main')
 
-    if name:
-        authenticator.logout('Logout', 'sidebar')
-        return True, name
-    else:
-        return False, None
+    # Das ist der Trick: Wir holen den Status aus dem session_state!
+    if "authentication_status" in st.session_state:
+        if st.session_state["authentication_status"]:
+            authenticator.logout('Logout', 'sidebar')
+            return True, name
+        elif st.session_state["authentication_status"] is False:
+            st.error("Benutzername oder Passwort falsch")
+        elif st.session_state["authentication_status"] is None:
+            st.warning("Bitte einloggen")
+    return False, None
