114381293eda0f4fad7aadbe4822953fda0ced65
Macrophage1 <johannes.zabka@icloud.com>
2025-06-04 23:29:56 +0200

    login.py

--- login.py
+++ login.py
@@ -4,6 +4,8 @@ import yaml
 from yaml.loader import SafeLoader
 
 def login():
+    st.write("Login-Funktion wurde aufgerufen")  # ← hier rein
+
     with open('config.yaml') as file:
         config = yaml.load(file, Loader=SafeLoader)
 
@@ -19,10 +21,12 @@ def login():
     if "authentication_status" in st.session_state:
         if st.session_state["authentication_status"]:
             authenticator.logout("Logout", "sidebar")
+            st.write("Login erfolgreich!")  # Debug
             return True, name
         elif st.session_state["authentication_status"] is False:
             st.error("Benutzername oder Passwort falsch")
         elif st.session_state["authentication_status"] is None:
             st.warning("Bitte einloggen")
 
+    st.write("Login fehlgeschlagen oder abgebrochen.")  # Debug
     return False, None
