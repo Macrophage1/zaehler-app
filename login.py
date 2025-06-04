6182efab5ddb0b92e7d2b6d2fa0b743de5228b12
Macrophage1 <johannes.zabka@icloud.com>
2025-06-04 23:25:51 +0200

    login.py

--- login.py
+++ login.py
@@ -16,12 +16,13 @@ def login():
 
     name = authenticator.login(location="main")
 
-if "authentication_status" in st.session_state:
-    if st.session_state["authentication_status"]:
-        authenticator.logout("Logout", "sidebar")
-        return True, name
-    elif st.session_state["authentication_status"] is False:
-        st.error("Benutzername oder Passwort falsch")
-    elif st.session_state["authentication_status"] is None:
-        st.warning("Bitte einloggen")
+    if "authentication_status" in st.session_state:
+        if st.session_state["authentication_status"]:
+            authenticator.logout("Logout", "sidebar")
+            return True, name
+        elif st.session_state["authentication_status"] is False:
+            st.error("Benutzername oder Passwort falsch")
+        elif st.session_state["authentication_status"] is None:
+            st.warning("Bitte einloggen")
 
+    return False, None
